1da7f31a7d2c8d02846b2250b2498e721492d759
zhangkuo <769329489@qq.com>
2025-08-08 17:14:32 +0800

    order 处理

--- backend/src/main/java/com/postion/airlineorderbackend/mapper/OrderMapper.java
+++ backend/src/main/java/com/postion/airlineorderbackend/mapper/OrderMapper.java
@@ -4,16 +4,13 @@ import com.postion.airlineorderbackend.dto.OrderDto;
 import com.postion.airlineorderbackend.model.Order;
 import com.postion.airlineorderbackend.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring") // Generates a Spring Bean for injection
 public interface OrderMapper {
 
     // MapStruct can map nested objects automatically if they have a corresponding mapping method
-    @Mappings({
-//            @Mapping(source = "author", target = "user"), // Map the user object
-//            @Mapping(target = "flightInfo", ignore = true) // flightInfo is added later, not from the entity
-    })
     OrderDto toDto(Order order);
 
     // This method will be used by the above toDto method to map the User entity to UserDto
